48e2a1c0344131047d0b7b2b3c3c9acf5cd4be96
The Admiral <chadcoleman1234@gmail.com>
2020-08-05 11:45:28 -0700

    buttons populating getting java error for 2nd on click

--- script.js
+++ script.js
@@ -3,68 +3,96 @@ var timer = document.querySelector("#timer");
 var startButton = document.querySelector("#startBtn");
 var questionContainer = document.querySelector("#QuestionCont");
 var ActiveQuestion = 0;
-var currentAnswer = 0;
+//var currentAnswer = 0;
 var startTime = 100 ;
 var time = "";
 var QuestionDisp = document.querySelector("#QuestionDisp")
 
-var Quest1 = 
-{
-    question: "What does CDN stand for?",
-    answers: ["Cached Data Now","Content Distribution Network","Content Delivery Network","Content Delivered Now"],
-    correctIndex: 2,
-    penaltyValue: 10
-}
-var Quest2 = 
-{
-    question: "Inside which HTML element do we put the JavaScript?",
-    answers: ["<script>","<scripting>","<js>","<javascript>"],
-    correctIndex: 0,
-    penaltyValue: 15
-}
-var Quest3 = 
-{
-    question: "What is the correct syntax for referring to an external script called xxx.js?",
-    answers: ["<script name=xxx.js>","<script href=xxx.js>","<script src=xxx.js>"],
-    correctIndex: 2,
-    penaltyValue: 7
-}
-var Quest4 = 
-{
-    question: "The external JavaScript file must contain the script tag.",
-    answers: ["True","False"],
-    correctIndex: 0,
-    penaltyValue: 5
-}
-var Quest5 = 
-{
-    question: "How do you write Hello World in an alert box?",
-    answers: ["alertbox(Hello World);","msg(Hello World);","alert(Hello World);","msgbox(Hello World);"],
-    correctIndex:2,
-    penaltyValue: 20
-}
-var Quest6 = 
-{
-    question: "How do you create a function in JavaScript?",
-    answers: ["function myFunction()","function = myFunction()","function:myFunction()","function myFunction"],
-    correctIndex: 0,
-    penaltyValue: 10
-}
-var Quest7 = 
-{
-    question: "How do you call a function named myFunction?",
-    answers: ["myFunction","call myFunction()","myFunction()","my.Function()"],
-    correctIndex: 2,
-    penaltyValue: 10
-}
-var Questions = [Quest1, Quest2, Quest3, Quest4, Quest5, Quest6, Quest7];
-//<----- Function for on click start---->
-//<--- Function to render Questions ---->
-function renderQuestion(){
-    if (ActiveQuestion <= Questions.length){
-        QuestionDisp.innerHTML = Questions[ActiveQuestion].question + "<hr>";
+var Questions = [
+
+    {
+        question: "What does CDN stand for?",
+        answers: ["Cached Data Now","Content Distribution Network","Content Delivery Network","Content Delivered Now"],
+        correctIndex: "Content Delivery Network",
+        
+    }, 
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: ["<script>","<scripting>","<js>","<javascript>"],
+        correctIndex: "<script>",
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called xxx.js?",
+        answers: ["<script name=xxx.js>","<script href=xxx.js>","<script src=xxx.js>"],
+        correctIndex: "<script src=xxx.js>",
+        
+    }, 
+    {
+        question: "The external JavaScript file must contain the script tag.",
+        answers: ["True","False"],
+        correctIndex: "True",
+        
+    },
+    {
+        question: "How do you write Hello World in an alert box?",
+        answers: ["alertbox(Hello World);","msg(Hello World);","alert(Hello World);","msgbox(Hello World);"],
+        correctIndex:"alert(Hello World);",
+        
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        answers: ["function myFunction()","function = myFunction()","function:myFunction()","function myFunction"],
+        correctIndex: "function myFunction()",
+        
+    },
+    {
+        question: "How do you call a function named myFunction?",
+        answers: ["myFunction","call myFunction()","myFunction()","my.Function()"],
+        correctIndex: "myFunction()",
+       
     }
-}
+];
+console.log(Questions[ActiveQuestion]);
+//<----- Function for on click start---->
+startButton.addEventListener("click", function(){
+    clearStart();
+    renderQuestionandAnswer();
+    //renderAnswers();
+       
+});
+function renderQuestionandAnswer(){
+    var currentAnswer = Questions[ActiveQuestion].answers;
+    QuestionDisp.innerHTML = Questions[ActiveQuestion].question + "<hr>";
+    for (let i = 0; i < currentAnswer.length; i++) {
+        document.getElementById("answer-0").innerHTML = currentAnswer[0];
+        document.getElementById("answer-1").innerHTML = currentAnswer[1];
+        document.getElementById("answer-2").innerHTML = currentAnswer[2];
+        document.getElementById("answer-3").innerHTML = currentAnswer[3];
+        };
+        var buttongrab = document.getElementsById("buttonclicks")
+        buttongrab.addEventListener("click", function(){
+            console.log("button clicked")
+        }
+        )};        
+
+function clearStart(){
+    var quiztitle = document.querySelector("#quizTitle");
+    var quizdescription =  document.querySelector("#quizDescription");
+    var startbuttoncont = document.querySelector("#startBtnCont");
+    var buttondiv = document.querySelector("#buttondiv");
+    quiztitle.classList.add("d-none");
+    quizdescription.classList.add("d-none");
+    startbuttoncont.classList.add("d-none");
+    buttondiv.classList.remove("d-none");
+  }
+
+
+
+
+
+
+    //<--- Function to render Questions ---->
+
 //<---- Render answers--->
 //function renderAnswers(){
     //    var answerList = document.querySelector("questions")
@@ -84,26 +112,16 @@ function renderQuestion(){
                 //        return;
                 //    });
                 //}
-                function renderAnswer(){
-                    var answerList = document.querySelector("#questions")
-                    var QuestAnswers = Questions.answers[0];
-                    QuestAnswers.forEach(function DisplayAnswer() {
-                        var answerbuttons = document.createElement("button")
-                        answerbuttons.innerHTML = QuestAnswers;
-                        answerList.innerHTML = QuestAnswers;
-                        return;  
-                    });
-                }
+                //function renderAnswer(){
+                //    var answerList = document.querySelector("#questions")
+                //    var QuestAnswers = Questions.answers[0];
+                //    QuestAnswers.forEach(function DisplayAnswer() {
+                //        var answerbuttons = document.createElement("button")
+                //        answerbuttons.innerHTML = QuestAnswers;
+                //        answerList.innerHTML = QuestAnswers;
+                //        return;  
+                //    });
+                //}
                 
-                startButton.addEventListener("click", function(){
-                    renderQuestion();
-                    clearStart();
-                    renderAnswers();
-                time = setInterval(tick,1000)    
-                });
-function clearStart(){
-document.querySelector("#quizTitle").classList.add("d-none");
-document.querySelector("#quizDescription").classList.add("d-none");
-document.querySelector("#startBtnCont").classList.add("d-none")
-}
+
 
